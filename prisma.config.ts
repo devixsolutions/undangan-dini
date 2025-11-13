0166f226ff5661acf979f620993725532295d731
aryacaesars <aryacaesars89@gmail.com>
2025-11-13 22:58:45 +0700

    Include Prisma client and query engine for Vercel

--- prisma.config.ts
+++ prisma.config.ts
@@ -10,4 +10,11 @@ export default defineConfig({
   datasource: {
     url: env("DATABASE_URL"),
   },
+  generator: {
+    client: {
+      provider: "prisma-client-js",
+      output: "./lib/generated/prisma",
+      binaryTargets: ["native", "rhel-openssl-3.0.x"],
+    },
+  },
 });
